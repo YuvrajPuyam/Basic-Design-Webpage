0eae6a8c1dafd7b45e568eadb170f7388b1e8591
YuvrajPuyam <yuvrajpuyam@gmail.com>
2020-04-14 16:13:14 +0530

    Commit #1

---
+++ main.js
@@ -0,0 +1,8 @@
+const toTop = document.querySelector(".back-to-top")
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 250){
+        toTop.classList.add("active")
+    }else{
+        toTop.classList.remove("active")
+    }
+})
